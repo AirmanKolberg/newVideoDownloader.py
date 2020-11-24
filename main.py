b736ed8160f467e645ccbb33df17072584c5604e
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-24 15:07:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from functions import *
+from secret_stuff import *
+from time import sleep
 
-# There is obviously a lot missing,
-# But I've recently updated the way
-# I'm searching YouTube for data, so
-# I shall be continuing to update as
-# time progresses, as per usual!
+# To find Channel IDs, visit the following page:
+# https://commentpicker.com/youtube-channel-id.php
 
-check_for_updates()
+bash_command('clear')
+last_viewed_channel_1 = check_for_updates(channel_id_1)
+last_viewed_channel_2 = check_for_updates(channel_id_2)
+new_video_channel_1 = False
+new_video_channel_2 = False
+
+
+def check_channel(channel_num_int, most_recent_url):
+    print(f'Last video viewed: {most_recent_url}')
+    new_vid_check = check_for_updates(channel_id_1)
+
+    if new_vid_check == most_recent_url:
+        sleep(15)
+    else:
+        print(f'New video for Channel {channel_num_int}: {new_vid_check}')
+
+
+while not new_video_channel_1 or not new_video_channel_2:
+    if not new_video_channel_1 and not new_video_channel_2:
+        check_channel(1, last_viewed_channel_1)
+        check_channel(2, last_viewed_channel_2)
+    elif not new_video_channel_1 and new_video_channel_2:
+        check_channel(1, last_viewed_channel_1)
+    elif new_video_channel_1 and not new_video_channel_2:
+        check_channel(2, last_viewed_channel_2)
+    else:
+        print('All channels have posted new videos.')
