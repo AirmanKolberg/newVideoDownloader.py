5fed36b21e1458cb60e4d546d7391e71547d7f9c
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-24 18:26:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 from functions import *
 from secret_stuff import *
-from time import sleep
 
 # To find Channel IDs, visit the following page:
 # https://commentpicker.com/youtube-channel-id.php
 
 bash_command('clear')
-last_viewed_channel_1 = check_for_updates(channel_id_1)
-last_viewed_channel_2 = check_for_updates(channel_id_2)
-new_video_channel_1 = False
-new_video_channel_2 = False
+last_viewed_channel_1 = check_for_updates(channel_1.id)
+last_viewed_channel_2 = check_for_updates(channel_2.id)
 
+print(f"""The last videos posted:
+{channel_1.name}: {last_viewed_channel_1}
+{channel_2.name}: {last_viewed_channel_2}""")
 
-def check_channel(channel_num_int, most_recent_url):
-    print(f'Last video viewed: {most_recent_url}')
-    new_vid_check = check_for_updates(channel_id_1)
-
-    if new_vid_check == most_recent_url:
-        sleep(15)
+while not channel_1.new_vid or not channel_2.new_vid:
+    if not channel_1.new_vid and not channel_2.new_vid:
+        channel_1.new_vid = check_channel(channel_1.name, channel_1.id, last_viewed_channel_1, False)
+        channel_2.new_vid = check_channel(channel_2.name, channel_2.id, last_viewed_channel_2, False)
+    elif not channel_1.new_vid and channel_2.new_vid:
+        channel_1.new_vid = check_channel(channel_1.name, channel_1.id, last_viewed_channel_1, False)
+    elif channel_1.new_vid and not channel_2.new_vid:
+        channel_2.new_vid = check_channel(channel_2.name, channel_2.id, last_viewed_channel_2, False)
     else:
-        print(f'New video for Channel {channel_num_int}: {new_vid_check}')
-
-
-while not new_video_channel_1 or not new_video_channel_2:
-    if not new_video_channel_1 and not new_video_channel_2:
-        check_channel(1, last_viewed_channel_1)
-        check_channel(2, last_viewed_channel_2)
-    elif not new_video_channel_1 and new_video_channel_2:
-        check_channel(1, last_viewed_channel_1)
-    elif new_video_channel_1 and not new_video_channel_2:
-        check_channel(2, last_viewed_channel_2)
-    else:
-        print('All channels have posted new videos.')
+        print('All YouTubers have new videos!')
