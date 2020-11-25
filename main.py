c4fa97d8d0e1017aee3f99fed097572148d29e91
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-24 19:21:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from functions import *
 from secret_stuff import *
 
-# To find Channel IDs, visit the following page:
-# https://commentpicker.com/youtube-channel-id.php
-
 bash_command('clear')
 last_viewed_channel_1 = check_for_updates(channel_1.id)
 last_viewed_channel_2 = check_for_updates(channel_2.id)
+last_viewed_channel_3 = check_for_updates(channel_3.id)
 
 print(f"""The last videos posted:
 {channel_1.name}: {last_viewed_channel_1}
-{channel_2.name}: {last_viewed_channel_2}""")
+{channel_2.name}: {last_viewed_channel_2}
+{channel_3.name}: {last_viewed_channel_3}""")
 
-while not channel_1.new_vid or not channel_2.new_vid:
-    if not channel_1.new_vid and not channel_2.new_vid:
-        channel_1.new_vid = check_channel(channel_1.name, channel_1.id, last_viewed_channel_1, False)
-        channel_2.new_vid = check_channel(channel_2.name, channel_2.id, last_viewed_channel_2, False)
-    elif not channel_1.new_vid and channel_2.new_vid:
-        channel_1.new_vid = check_channel(channel_1.name, channel_1.id, last_viewed_channel_1, False)
-    elif channel_1.new_vid and not channel_2.new_vid:
-        channel_2.new_vid = check_channel(channel_2.name, channel_2.id, last_viewed_channel_2, False)
+while not channel_1.new_vid or not channel_2.new_vid or not channel_3.new_vid:
+    check_channel(channel_1.name, channel_1.id, last_viewed_channel_1, channel_1.new_vid)
+    check_channel(channel_2.name, channel_2.id, last_viewed_channel_2, channel_2.new_vid)
+    check_channel(channel_3.name, channel_3.id, last_viewed_channel_3, channel_3.new_vid)
+
+print('All YouTubers have new videos!')
+downloaded = False
+
+while not downloaded:
+    download_request = input('Do you wish to download the new videos for later viewing? ').lower()
+
+    if download_request == 'yes':
+        for link in new_video_list:
+            download_youtube_video(link)
+            downloaded = True
+    elif download_request == 'no':
+        print('Have a nice day!')
     else:
-        print('All YouTubers have new videos!')
+        print(f'{download_request} is neither yes, nor no.  Please try again.')
